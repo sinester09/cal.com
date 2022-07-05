aefb6ff02647d0a28352bb4d6261fa81d1656e4a
sinester09 <60792457+sinester09@users.noreply.github.com>
2022-07-05 10:53:07 -0500

    error constant
    mejora error constant

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -1,4 +1,4 @@
-export const WEBAPP_URL = process.env.NEXT_PUBLIC_WEBAPP_URL || `https://${process.env.VERCEL_URL}`;
+
 const VERCEL_URL = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "";
 const RAILWAY_STATIC_URL = process.env.RAILWAY_STATIC_URL ? `https://${process.env.RAILWAY_STATIC_URL}` : "";
 const HEROKU_URL = process.env.HEROKU_APP_NAME ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com` : "";
